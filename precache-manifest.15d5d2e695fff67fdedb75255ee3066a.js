1830ec2f977218edc92c6b1c045c586a1fc5ed68
ruslanofc <ruslanofcourse@gmail.com>
2019-12-05 22:07:46 +0300

    Updates

---
+++ precache-manifest.15d5d2e695fff67fdedb75255ee3066a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f217ab2def12dfeb2ceb70c00031281",
+    "url": "https//ruslanofc.github.io/shitest/index.html"
+  },
+  {
+    "revision": "e1bf26d00f38ebb8730a",
+    "url": "https//ruslanofc.github.io/shitest/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b2b5f56882e2eabeb53d",
+    "url": "https//ruslanofc.github.io/shitest/static/js/2.314647af.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "https//ruslanofc.github.io/shitest/static/js/2.314647af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1bf26d00f38ebb8730a",
+    "url": "https//ruslanofc.github.io/shitest/static/js/main.c0c9d1ae.chunk.js"
+  },
+  {
+    "revision": "eaf9fe34fceb6e38b915",
+    "url": "https//ruslanofc.github.io/shitest/static/js/runtime-main.2e424635.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "https//ruslanofc.github.io/shitest/static/media/logo.5d5d9eef.svg"
+  }
+]);
